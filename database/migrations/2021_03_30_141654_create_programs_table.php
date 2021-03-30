9e10095100e6754e9fbd7ee5fe2f7e6a3c325ee7
Kevin Kayisire <kaykevin1@gmail.com>
2021-03-30 16:28:04 +0200

    Added Product

---
+++ database/migrations/2021_03_30_141654_create_programs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('picture_url');
+            $table->integer('status');
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
---
+++ database/migrations/2021_03_30_141839_create_program_content_uploads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramContentUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('program_content_uploads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('upload_url');
+            $table->integer('status');
+            $table->foreignId('program_content_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('program_content_uploads');
+    }
+}
