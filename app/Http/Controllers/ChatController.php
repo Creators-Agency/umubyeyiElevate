7c10ad29539bd785f4a979c8aa31c73d12e7a136
tinoxn <niyonshutivalentin@gmail.com>
2021-07-01 22:04:58 +0200

    updates

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -226,17 +226,20 @@ class ChatController extends Controller
         $subchat = [];
         $programs = Program::get();
         foreach($programs as $program){
-              $chats = DB::table("chats")
+                $chats = DB::table("chats")
                     ->join("chat_users","chats.id","chat_users.chat_id")
                     ->join("users","chat_users.user_id","users.id")
                     ->where("chat_users.user_id",$user)
                     // ->where("chat_users.status",1)
                     ->where("chats.program_id",$program->id)
                     ->select(
-                        "chat_users.chat_id as chat"
+                        "chat_users.chat_id as id"
                     )
-                    ->get();
-                    array_push($subchat,$chats);
+                    ->first();
+                    if ($chats) {
+                        array_push($subchat,$chats);
+                    }
+                    
             
         }
         $singles = Chat::get();
