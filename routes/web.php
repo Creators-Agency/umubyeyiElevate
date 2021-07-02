44c497d0de80dd5ce8aee483cdcd0b95629cb097
tinoxn <niyonshutivalentin@gmail.com>
2021-07-02 08:32:30 +0200

    updates

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,20 @@ Route::prefix('admin/category')->group(function () {
     Route::put('/{id}', [PriviledgeController::class, 'update']);
     Route::delete('/{id}', [PriviledgeController::class, 'destroy']);
 });
+
+Route::get('/dev-1234',function()
+    {
+        $alldata=[];
+
+        Schema::table('menus', function($table) use ($alldata)
+        {
+            /**
+             * payouts
+             * solar_panels
+             */
+
+            $table->longText('description')->after('menu_name');
+            // $table->integer('moreInfo')->after('status')->default(0);
+        });
+    }
+);
