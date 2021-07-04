324a217b4087209b9d71433627026e24f621ecaa
tinoxn <niyonshutivalentin@gmail.com>
2021-07-04 17:21:59 +0200

    updates

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -14,7 +14,20 @@ class MessageController extends Controller
 {
     public function index($chat_id)
     {
-        $message = ChatMessage::where('chat_id',$chat_id)->get();
+        // $message = ChatMessage::where('chat_id',$chat_id)->get();
+        $message = DB::table('chat_messages')
+                    ->join("users","users.id","chat_messages.user_id")
+                    ->where('chat_messages.chat_id',$chat_id)
+                    ->select(
+                        "chat_messages.id as id",
+                        "chat_messages.content as content",
+                        "chat_messages.chat_id as chat_id",
+                        "chat_messages.user_id as user_id",
+                        "chat_messages.created_at as created_at",
+                        "chat_messages.updated_at as updated_at",
+                        "users.name as name",
+                    )
+                    ->get();
         return response()->json([
             "message" => "all message from chat ID",
             "payload" => $message,
